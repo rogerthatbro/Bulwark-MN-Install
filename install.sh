a6d5b6e66abb20782b6d82bf6f92633826eacaf9
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2017-12-05 22:10:18 +1000

    changed outputs to echo

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
-﻿output() {
-    printf "\E[0;33;40m"
-    echo $1
-    printf "\E[0m"
-}
-
-displayErr() {
-    echo
-    echo $1;
-    echo
-    exit 1;
-}
 clear
-output "Make sure you double check before hitting enter! Only one shot at these!"
-output ""
+# declare STRING variable
+STRING1="Make sure you double check before hitting enter! Only one shot at these!"
+STRING2="If you found this helpful, please donate to BWK Donation: "
+STRING3="bCkL87UvfwqphwCdWgyShFYz54hgPVJAg3"
+STRING4="Updating system and installing required packages."
+STRING5="Switching to Aptitude"
+STRING6="Some optional installs"
+STRING7="Start your masternode"
+STRING8="Now, you need to finally start these things in this order"
+STRING9="Go to your windows wallet and from the Control wallet debug console please enter"
+STRING10="startmasternode alias false <mymnalias>"
+STRING11="where <mymnalias> is the name of your masternode alias (without brackets)"
+STRING12="once completed please return to VPS and press the space bar"
+
+
+#print variable on a screen
+echo $STRING1 
+
     read -e -p "Server IP Address : " ip
     read -e -p "Masternode Private Key (e.g. 7edfjLCUzGczZi3JQw8GHp434R9kNY33eFyMGeKRymkB56G4324h # THE KEY YOU GENERATED EARLIER) : " key
     read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
     read -e -p "Install UFW and configure ports? [Y/n] : " UFW
 
     clear
-    output "If you found this helpful, please donate to BWK Donation: "
-    output ""
-    output "bCkL87UvfwqphwCdWgyShFYz54hgPVJAg3"
-    output ""
-    output "Updating system and installing required packages."
-    output ""
+ echo $STRING2    
+ echo $STRING3    
+ echo $STRING4    
+    sleep 10    
 
 # update package and upgrade Ubuntu
     sudo apt-get -y update
@@ -32,17 +34,17 @@ output ""
     sudo apt-get -y autoremove
     sudo apt-get install wget nano htop -y
     clear
-    output "Switching to Aptitude"
-    output ""
+echo $STRING5
     sudo apt-get -y install aptitude
 
 #Generating Random Passwords
     password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
     password2=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 
-    output "Some optional installs"
+echo $STRING6
     if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
     sudo aptitude -y install fail2ban
+    sudo service fail2ban restart 
     fi
     if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
     sudo apt-get install ufw
@@ -50,6 +52,7 @@ output ""
     sudo ufw default allow outgoing
     sudo ufw allow ssh
     sudo ufw allow 52543/tcp
+    sudo ufw enable -y
     fi
 
 #Install Bulwark Daemon
@@ -60,13 +63,12 @@ output ""
     sudo cp ~/bulwark-1.2.0/bin/bulwark-cli /usr/bin
     bulwarkd -daemon
     clear
+    
 #Setting up coin
-    output "If you found this helpful, please donate to BWK Donation: "
-    output ""
-    output "bCkL87UvfwqphwCdWgyShFYz54hgPVJAg3"
-    output ""
-    output "Updating system and installing required packages."
-    output ""
+echo $STRING2
+echo $STRING3
+echo $STRING4
+sleep 10
 
 #Create bulwark.conf
 echo '
@@ -87,21 +89,18 @@ masternode=1
     sudo chmod 0600 ~/.bulwark/bulwark.conf
 
 #Starting coin
-    (crontab -l 2>/dev/null; echo '@reboot sleep 60 && bulwarkd -daemon -shrinkdebugfile') | crontab
+    (crontab -l 2>/dev/null; echo '@reboot sleep 30 && bulwarkd -daemon -shrinkdebugfile') | crontab
+    (crontab -l 2>/dev/null; echo '@reboot sleep 60 && bulwark-cli startmasternode local false') | crontab
     bulwarkd -daemon
 
-    output "Start your masternode"
-    output ""
-    output "Now, you need to finally start these things in this order"
-    output ""
-    output "Go to your windows wallet and from the Control wallet debug console please enter"
-    output ""
-    output "startmasternode alias false <mymnalias>"
-    output ""
-    output "where <mymnalias> is the name of your masternode alias (without brackets)"
-    output ""
-    output "once completed please return to VPS and press the space bar"
-    output ""
+echo $STRING7
+echo $STRING8 
+echo $STRING9 
+echo $STRING10
+echo $STRING11
+echo $STRING12
+    sleep 10
+    
     read -p "Press any key to continue... " -n1 -s
     bulwark-cli startmasternode local false
     bulwark-cli masternode status
