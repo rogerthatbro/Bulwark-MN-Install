ce64013c39c01bbe1e002a36aa2f43342e3f51a4
Bulwark Cryptocurrency <32829172+bulwark-crypto@users.noreply.github.com>
2018-02-06 14:13:30 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ echo $STRING1
     sudo apt-get -y upgrade
     sudo apt-get -y autoremove
     sudo apt-get install wget nano htop -y
-    sudo apt-get install build-essential && sudo apt-get install libtool autotools-dev autoconf automake && sudo apt-get install libssl-dev && sudo apt-get install libboost-all-dev && sudo apt install software-properties-common && sudo add-apt-repository ppa:bitcoin/bitcoin && sudo apt update && sudo apt-get install libdb4.8-dev && sudo apt-get install libdb4.8++-dev && sudo apt-get install libminiupnpc-dev && sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler && sudo apt-get install libqrencode-dev && sudo apt-get install -y git && sudo apt-get install pkg-config
+    sudo apt-get -y install build-essential && sudo apt-get install libtool autotools-dev autoconf automake && sudo apt-get install libssl-dev && sudo apt-get install libboost-all-dev && sudo apt install software-properties-common && sudo add-apt-repository ppa:bitcoin/bitcoin && sudo apt update && sudo apt-get install libdb4.8-dev && sudo apt-get install libdb4.8++-dev && sudo apt-get install libminiupnpc-dev && sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler && sudo apt-get install libqrencode-dev && sudo apt-get install -y git && sudo apt-get install pkg-config
     clear
 echo $STRING5
     sudo apt-get -y install aptitude
