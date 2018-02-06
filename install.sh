d461b916ef58afdcb3b6f98b6684be50484b68d7
Bulwark Cryptocurrency <32829172+bulwark-crypto@users.noreply.github.com>
2018-02-06 12:36:11 +1000

    Longest Chain

--- install.sh
+++ install.sh
@@ -60,11 +60,11 @@ echo $STRING6
     fi
 
 #Install Bulwark Daemon
-    wget https://github.com/bulwark-crypto/Bulwark/releases/download/bulwark-1.2.0.3/bulwark-1.2.0.3-x86_64-ubuntu16.04-gnu.tar.gz
-    sudo tar -xzvf bulwark-1.2.0.3-x86_64-ubuntu16.04-gnu.tar.gz
-    sudo rm bulwark-1.2.0.3-x86_64-ubuntu16.04-gnu.tar.gz
-    sudo cp ~/bulwark-1.2.0/bin/bulwarkd /usr/bin
-    sudo cp ~/bulwark-1.2.0/bin/bulwark-cli /usr/bin
+    wget https://github.com/bulwark-crypto/Bulwark/releases/download/1.2.1.0/Bulwark121.tgz
+    sudo tar -xzvf Bulwark121.tgz
+    sudo rm Bulwark121.tgz
+    sudo cp bulwarkd /usr/bin
+    sudo cp bulwark-cli /usr/bin
     sudo cp ~/Bulwark-MN-Install/bulwark-1.2.0/bin/bulwarkd /usr/bin
     sudo cp ~/Bulwark-MN-Install/bulwark-1.2.0/bin/bulwark-cli /usr/bin
     bulwarkd -daemon
@@ -94,6 +94,31 @@ bind='$ip':52543
 masternodeaddr='$ip'
 masternodeprivkey='$key'
 masternode=1
+addnode=213.32.23.65:52543
+addnode=199.247.7.119:52543
+addnode=185.223.30.31:52543
+addnode=80.209.229.92:52543
+addnode=45.33.58.91:52543
+addnode=46.101.131.50:52543
+addnode=159.203.123.44:52543
+addnode=209.250.232.68:42870
+addnode=199.247.4.86:39654
+addnode=109.101.221.204:57374
+addnode=46.149.120.21:53380
+addnode=86.149.228.161:54055
+addnode=45.77.138.177:52543
+addnode=207.148.1.60:52543
+addnode=114.94.190.239:54954
+addnode=45.63.49.239:38716
+addnode=80.122.43.78:52543
+addnode=45.32.221.80:52543
+addnode=108.61.188.251:52543
+addnode=73.73.160.113:51356
+addnode=134.255.252.218:55912
+addnode=104.238.157.205:52543
+addnode=45.76.118.238:52543
+addnode=47.88.220.108:52543
+addnode=104.156.225.96:52543
 ' | sudo -E tee ~/.bulwark/bulwark.conf >/dev/null 2>&1
     sudo chmod 0600 ~/.bulwark/bulwark.conf
 
