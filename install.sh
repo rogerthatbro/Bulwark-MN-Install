4760c9c0fd5782cc0308499652dc802617925e5e
padraiq <padraiq@gmail.com>
2018-02-08 06:01:00 +0000

    Update to 1.2.1.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-# declare STRING variable
+
 STRING1="Make sure you double check before hitting enter! Only one shot at these!"
 STRING2="If you found this helpful, please donate to BWK Donation: "
 STRING3="bCkL87UvfwqphwCdWgyShFYz54hgPVJAg3"
@@ -14,66 +14,65 @@ STRING10="startmasternode alias false <mymnalias>"
 STRING11="where <mymnalias> is the name of your masternode alias (without brackets)"
 STRING12="once completed please return to VPS and press the space bar"
 STRING13=""
+STRING14="Please Wait a minimum of 15 minutes before proceeding, the node wallet must be synced"
 
-#print variable on a screen
-echo $STRING1 
+echo $STRING1
 
-    read -e -p "Server IP Address : " ip
-    read -e -p "Masternode Private Key (e.g. 7edfjLCUzGczZi3JQw8GHp434R9kNY33eFyMGeKRymkB56G4324h # THE KEY YOU GENERATED EARLIER) : " key
-    read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
-    read -e -p "Install UFW and configure ports? [Y/n] : " UFW
+read -e -p "Server IP Address : " ip
+read -e -p "Masternode Private Key (e.g. 7edfjLCUzGczZi3JQw8GHp434R9kNY33eFyMGeKRymkB56G4324h # THE KEY YOU GENERATED EARLIER) : " key
+read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
+read -e -p "Install UFW and configure ports? [Y/n] : " UFW
 
-    clear
- echo $STRING2
- echo $STRING13
- echo $STRING3 
- echo $STRING13
- echo $STRING4    
-    sleep 10    
+clear
+echo $STRING2
+echo $STRING13
+echo $STRING3
+echo $STRING13
+echo $STRING4
+sleep 10
 
 # update package and upgrade Ubuntu
-    sudo apt-get -y update
-    sudo apt-get -y upgrade
-    sudo apt-get -y autoremove
-    sudo apt-get install wget nano htop -y
-    sudo apt-get -y install build-essential && sudo apt-get install libtool autotools-dev autoconf automake && sudo apt-get install libssl-dev && sudo apt-get install libboost-all-dev && sudo apt install software-properties-common && sudo add-apt-repository ppa:bitcoin/bitcoin && sudo apt update && sudo apt-get install libdb4.8-dev && sudo apt-get install libdb4.8++-dev && sudo apt-get install libminiupnpc-dev && sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler && sudo apt-get install libqrencode-dev && sudo apt-get install -y git && sudo apt-get install pkg-config
-    sudo apt-get -y install libzmq3-dev
-    clear
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get -y install build-essential libtool autotools-dev autoconf automake libssl-dev libboost-all-dev software-properties-common libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libqt4-dev libprotobuf-dev protobuf-compiler libqrencode-dev git pkg-config libzmq3-dev wget nano htop
+clear
 echo $STRING5
-    sudo apt-get -y install aptitude
+sudo apt-get -y install aptitude
 
 #Generating Random Passwords
-    password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-    password2=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+password2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 echo $STRING6
-    if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
-    cd ~
-    sudo aptitude -y install fail2ban
-    sudo service fail2ban restart 
-    fi
-    if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
-    sudo apt-get install ufw
-    sudo ufw default deny incoming
-    sudo ufw default allow outgoing
-    sudo ufw allow ssh
-    sudo ufw allow 52543/tcp
-    sudo ufw enable -y
-    fi
+if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
+  cd ~
+  sudo aptitude -y install fail2ban
+  sudo service fail2ban restart
+fi
+if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
+  sudo apt-get install ufw
+  sudo ufw default deny incoming
+  sudo ufw default allow outgoing
+  sudo ufw allow ssh
+  sudo ufw allow 52543/tcp
+  sudo ufw enable -y
+fi
 
 #Install Bulwark Daemon
-    wget https://github.com/bulwark-crypto/Bulwark/releases/download/1.2.1.0/Bulwark121.tgz
-    sudo tar -xzvf Bulwark121.tgz
-    sudo rm Bulwark121.tgz
-    sudo cp bulwarkd /usr/bin
-    sudo cp bulwark-cli /usr/bin
-    sudo cp ~/Bulwark-MN-Install/bulwark-1.2.0/bin/bulwarkd /usr/bin
-    sudo cp ~/Bulwark-MN-Install/bulwark-1.2.0/bin/bulwark-cli /usr/bin
-    bulwarkd -daemon
-    clear
-    
+wget https://github.com/bulwark-crypto/Bulwark/releases/download/1.2.1.0/bulwark-1.2.1.0-x86_64-pc-linux-gnu.tar.gz
+sudo tar -xzvf bulwark-1.2.1.0-x86_64-pc-linux-gnu.tar.gz
+sudo rm bulwark-1.2.1.0-x86_64-pc-linux-gnu.tar.gz
+sudo cp ~/bulwark-1.2.1/bin/bulwarkd /usr/bin
+sudo cp ~/bulwark-1.2.1/bin/bulwark-cli /usr/bin
+sudo cp ~/Bulwark-MN-Install/bulwark-1.2.1/bin/bulwarkd /usr/bin
+sudo cp ~/Bulwark-MN-Install/bulwark-1.2.1/bin/bulwark-cli /usr/bin
+bulwarkd -daemon
+clear
+
 #Setting up coin
-    clear
+clear
 echo $STRING2
 echo $STRING13
 echo $STRING3
@@ -122,33 +121,40 @@ addnode=45.76.118.238:52543
 addnode=47.88.220.108:52543
 addnode=104.156.225.96:52543
 ' | sudo -E tee ~/.bulwark/bulwark.conf >/dev/null 2>&1
-    sudo chmod 0600 ~/.bulwark/bulwark.conf
+sudo chmod 0600 ~/.bulwark/bulwark.conf
 
 #Starting coin
-    (crontab -l 2>/dev/null; echo '@reboot sleep 30 && bulwarkd -daemon -shrinkdebugfile') | crontab
-    (crontab -l 2>/dev/null; echo '@reboot sleep 60 && bulwark-cli startmasternode local false') | crontab
-    bulwarkd -daemon
+(
+  crontab -l 2>/dev/null
+  echo '@reboot sleep 30 && bulwarkd -daemon -shrinkdebugfile'
+) | crontab
+(
+  crontab -l 2>/dev/null
+  echo '@reboot sleep 60 && bulwark-cli startmasternode local false'
+) | crontab
+bulwarkd -daemon
 
-    clear
+clear
 echo $STRING2
 echo $STRING13
 echo $STRING3
 echo $STRING13
 echo $STRING4
-    sleep 10
+sleep 10
 echo $STRING7
 echo $STRING13
-echo $STRING8 
+echo $STRING8
 echo $STRING13
-echo $STRING9 
+echo $STRING9
 echo $STRING13
 echo $STRING10
 echo $STRING13
 echo $STRING11
 echo $STRING13
 echo $STRING12
-    sleep 120
-    
-    read -p "Press any key to continue... " -n1 -s
-    bulwark-cli startmasternode local false
-    bulwark-cli masternode status
+echo $STRING14
+sleep 15m
+
+read -p "Press any key to continue... " -n1 -s
+bulwark-cli startmasternode local false
+bulwark-cli masternode status
