496b816da8b8bc9a488efe961746d6d6689818b8
padraiq <padraiq@gmail.com>
2018-02-09 21:57:33 +0000

    Update refresh_node.sh

--- refresh_node.sh
+++ refresh_node.sh
@@ -10,6 +10,7 @@ rm -rf ~/.bulwark/database
 rm -rf ~/.bulwark/chainstate
 rm -rf ~/.bulwark/peers.dat
 
+cp ~/.bulwark/bulwark.conf ~/.bulwark/bulwark.conf.backup
 sed -i '/^addnode/d' ~/.bulwark/bulwark.conf
 cat <<EOL >>  ~/.bulwark/bulwark.conf
 
