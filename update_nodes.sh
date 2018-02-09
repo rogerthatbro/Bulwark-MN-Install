85e54619b36ecb44a3cab42ec208f6d5cc5a6ae8
padraiq <padraiq@gmail.com>
2018-02-09 21:51:52 +0000

    Update update_nodes.sh

--- update_nodes.sh
+++ update_nodes.sh
@@ -3,9 +3,15 @@ clear
 
 killall -9 bulwarkd
 
-sed -i '/addnode/d' ~/.bulwark/bulwark.conf
+rm -rf ~/.bulwark/blocks
+rm -rf ~/.bulwark/database
+rm -rf ~/.bulwark/chainstate
+rm -rf ~/.bulwark/peers.dat
 
-cat <<EOF > ~/.bulwark/bulwark.conf
+#!/bin/bash
+sed -i '/^addnode/d' ~/.bulwark/bulwark.conf
+cat <<EOL >>  ~/.bulwark/bulwark.conf
+
 addnode=136.243.60.144:52543
 addnode=172.104.36.68:52543
 addnode=195.181.214.183:52543
@@ -16,8 +22,6 @@ addnode=[2001:0:9d38:6ab8:c8e:2776:4deb:4d85]:61638
 addnode=[2001:0:9d38:6ab8:f0:2b42:fda0:4564]:52543
 addnode=[2001:0:9d38:6abd:18f6:fbff:2c24:b9f9]:63076
 addnode=[2a01:4f8:13b:1110::2]:49934
-EOF
-sudo chmod 0600 ~/.bulwark/bulwark.conf
+EOL
 
-bulwark-cli stop
 bulwarkd -daemon
