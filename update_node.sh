c62540b3c18737412d234d826418b7061d7feead
KaneoHunter <dingdongkane@gmail.com>
2018-03-20 01:20:00 +0000

    words

--- update_node.sh
+++ update_node.sh
@@ -5,14 +5,14 @@ TARBALLNAME="bulwark-1.2.3.0-linux64.tar.gz"
 BWKVERSION="1.2.3.0"
 
 clear
-echo "This script will update your masternode to version 1.2.2."
+echo "This script will update your masternode to version 1.2.3."
 read -p "Press Ctrl-C to abort or any other key to continue. " -n1 -s
 clear
 echo "Please enter your password to enter administrator mode:"
 sudo true
 echo "Shutting down masternode..."
 bulwark-cli stop
-echo "Installing Bulwark 1.2.2..."
+echo "Installing Bulwark 1.2.3..."
 mkdir ./bulwark-temp && cd ./bulwark-temp
 wget $TARBALLURL
 tar -xzvf $TARBALLNAME && mv bin bulwark-$BWKVERSION
