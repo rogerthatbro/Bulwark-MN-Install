9be4e8f5fbfe5f40533b188480e85d7b55fdc6e8
KaneoHunter <dingdongkane@gmail.com>
2018-06-24 04:39:31 +0100

    Update refresh_node.sh

--- refresh_node.sh
+++ refresh_node.sh
@@ -48,7 +48,7 @@ clear
 echo "Your masternode is syncing. Please wait for this process to finish."
 echo "This can take up to a few hours. Do not close this window." && echo ""
 
-until [ -n "$(bulwark-cli getconnectioncount 2>/dev/null)"  ]
+until [ -n "$(bulwark-cli getconnectioncount 2>/dev/null)"  ]; do
   sleep 1
 done
 
