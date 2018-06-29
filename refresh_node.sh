ac012f1d6800687d1ee6822bc48897eb06d033bd
kewagi <kewagi@icloud.com>
2018-06-29 15:26:48 +0900

    Added apt update, tweaked some things

--- refresh_node.sh
+++ refresh_node.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Make sure curl is installed
-sudo apt -qqy install curl
+apt-get -qq update
+apt -qqy install curl
 clear
 
 BOOTSTRAPURL=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep bootstrap.dat.xz | grep browser_download_url | cut -d '"' -f 4`
